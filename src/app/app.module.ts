84a79bea47c7178bd1c25b9a1fff2f0a0245b75b
Jesus <dolpsdw@gmail.com>
2019-12-04 14:30:41 +0100

    component with scss used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SportsbookModule } from 'sportsbook';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SportsbookModule
   ],
   providers: [],
   bootstrap: [AppComponent]
